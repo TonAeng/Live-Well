4260601b62cb21a0832a3a7208e419e6c718c70c
Thobi <jirayoot537@gmail.com>
2023-03-16 22:46:44 +0700

    update

--- lib/query/insert.dart
+++ lib/query/insert.dart
@@ -21,9 +21,10 @@ class _InsertState extends State<Insert> {
     super.initState();
      
   }
-  void _addUserDataToFirestore(String name ,String age) async {
+  void _addUserDataToFirestore(String name, double age) async {
   await _firestore.collection('user').add({'name': name , 'age': age });
 }
+
 
   @override
   Widget build(BuildContext context) {
@@ -31,20 +32,19 @@ class _InsertState extends State<Insert> {
       body: SafeArea(
           child: Column(
         children: [
-          TextField(
+           TextField(
             controller: nameController,
             decoration: InputDecoration(label: Text("Name")),
           ),
           TextField(
             controller: ageController,
             decoration: InputDecoration(label: Text("Age")),
+            keyboardType: TextInputType.numberWithOptions(decimal: true), // เพิ่ม keyboardType เป็น number และกำหนดให้รองรับ decimal
           ),
            ElevatedButton(
   onPressed: () {
-      _addUserDataToFirestore(nameController.text, ageController.text);
-  
-    
-  },
+              _addUserDataToFirestore(nameController.text, double.parse(ageController.text)); // แปลง String เป็น double ด้วย double.parse()
+            },
   child: Text('Add'),
 )
         ],
